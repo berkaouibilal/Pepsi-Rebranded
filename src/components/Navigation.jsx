1cab90096424919b398e3b5254169108243ae0aa
Bilal Berkaoui <berkaouibilal@mail.ru>
2025-02-21 20:14:46 +0100

    Fixed the routing to Home

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -242,7 +242,8 @@ const Navigation = ({ theme, toggleTheme }) => {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: index * 0.1, duration: 0.5 }}
               >
-                <NavLink to={`/${item.toLowerCase()}`} theme={theme}>
+                <NavLink to={`item === "Home" ? "/" : `/${item.toLowerCase()}`} 
+                theme={theme}>
                   {item}
                 </NavLink>
               </motion.div>
